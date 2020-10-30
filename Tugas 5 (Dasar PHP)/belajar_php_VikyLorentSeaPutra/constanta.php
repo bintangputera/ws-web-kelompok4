56501f475f652a63e6fbe018e1b1f06faa5b11d7
viky2311 <54499029+viky2311@users.noreply.github.com>
2020-10-30 14:46:24 +0700

    Add files via upload

---
+++ Tugas 5 (Dasar PHP)/belajar_php_VikyLorentSeaPutra/constanta.php
@@ -0,0 +1,15 @@
+<?php
+
+// Membuat constanta
+define('VERSION', '1.0.0' );
+
+const SITE_NAME ="Petani Kode";
+const BASE_URL ="https://www.petanikode.com";
+
+
+// Cetak nilai constanta
+
+echo "Site name = " .SITE_NAME. "</br>";
+echo "URL = " .BASE_URL. "</br>";
+echo "Version = " .VERSION. "</br>";
+?>
---
+++ Tugas 5 (Dasar PHP)/belajar_php_VikyLorentSeaPutra/variable.php
@@ -0,0 +1,9 @@
+<?php
+
+$namaBarang="Minyak Goreng";
+$harga= 15000;
+
+
+echo "ibu membeli $namaBarang dengan $harga";
+
+?>
