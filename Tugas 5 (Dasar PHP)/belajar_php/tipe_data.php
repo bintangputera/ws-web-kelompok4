149abf55c5630eeb2be87a82265e842688a48750
viky2311 <54499029+viky2311@users.noreply.github.com>
2020-10-30 14:43:43 +0700

    Add files via upload

---
+++ Tugas 5 (Dasar PHP)/belajar_php/tipe_data.php
@@ -0,0 +1,29 @@
+<?php
+
+// tipe data char (karakter)
+$jenisKelamin ="L";
+
+// tipe data teks (string)
+$namaLengkap="Petani Kode";
+
+// tipe data int
+$umur=20;
+
+// tipe data float
+$berat=49.5;
+
+// tipe data float
+$tinggi=182.2;
+
+// tipe data boolean
+$menikah= false;
+
+
+echo "Nama = $namaLengkap <br>";
+echo "Jenis Kelamin = $jenisKelamin <br>";
+echo "Umur = $umur <br>";
+echo "Berat Badan = $berat <br>";
+echo "Tinggi Badan = $tinggi <br>";
+echo "Menikah = $menikah <br>";
+
+?>
