1fa2ea5dc3e1d08852ba37d785f24cd99030a4c1
Naufal Rafif <66282756+Veinbos@users.noreply.github.com>
2020-10-30 11:42:54 +0700

    Add files via upload

---
+++ Tugas 5 (Dasar PHP)/belajar_php_Naufal Rafif/constanta.php
@@ -0,0 +1,12 @@
+<?php
+
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "Petadikode";
+const BASE_URL = "https://www.petanikode.com";
+
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
+
+?>
