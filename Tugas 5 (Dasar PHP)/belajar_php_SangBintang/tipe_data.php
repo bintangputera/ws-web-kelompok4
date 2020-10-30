3bef33466b40d8d45cab6bd0fbb81d173d92647f
bintangputera <sangbintang917@gmail.com>
2020-10-30 09:00:49 +0700

    Mengerjakan Tugas 5 - Sang Bintang

---
+++ Tugas 5 (Dasar PHP)/belajar_php_SangBintang/tipe_data.php
@@ -0,0 +1,28 @@
+<?php
+
+// tipe data char
+$jenis_kelamin = 'L';
+
+// tipe data string
+$nama_lengkap = "Petani Kode";
+
+// tipe data integer
+$umur = 20;
+
+// tipe data float
+$berat = 48.3;
+
+// tipe data float
+$tinggi = 182.2;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama : $nama_lengkap<br>";
+echo "Jenis Kelamin : $jenis_kelamin<br>";
+echo "Umur : $umur<br>";
+echo "Berat badan : $berat<br>";
+echo "Tinggi badang : $tinggi<br>";
+echo "Menikah : $menikah<br>";
+
+?>
