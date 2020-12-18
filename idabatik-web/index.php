0b0d52a90df6179af30a4fe3ff0c0c00fa9b00c1
viky2311 <54499029+viky2311@users.noreply.github.com>
2020-12-18 11:09:56 +0700

    mengerjakan galeri dan ig

--- idabatik-web/index.php
+++ idabatik-web/index.php
@@ -5,6 +5,7 @@
     $query = mysqli_query($mysqli, "SELECT * FROM slider_gallery");
 ?>
 <?php include "partial/header-html.php" ?>
+
 </head>
 
 <body>
@@ -22,7 +23,7 @@
         <div class="hero-items owl-carousel">
             <?php 
                 while ($gallery = mysqli_fetch_array($query)) {
-                
+ 
             ?>
             <div class="single-hero-items set-bg" data-setbg="<?= $_ENV['base_url'].'gallery/'.$gallery['file'] ?>">
                 <div class="container"s>
@@ -46,7 +47,7 @@
             <div class="row">
                 <div class="col-lg-4">
                     <div class="single-banner">
-                        <img src="img/banner-1.jpg" alt="">
+                    <img  src="<?= $_ENV['base_url']."img-uploaded/874-3e2a8d29878e5e57215658768bb480b9.jpg"?>" alt="">
                         <div class="inner-text">
                             <h4>Men’s</h4>
                         </div>
@@ -74,7 +75,7 @@
     <!-- Banner Section End -->
 
     <!-- Women Banner Section Begin -->
-    <section class="women-banner spad">
+    <!-- <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-3">
@@ -187,7 +188,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
     <!-- Women Banner Section End -->
 
     <!-- Man Banner Section Begin -->
@@ -195,7 +196,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-8">
-                    <div class="filter-control">
+                    <div class="filter-control">    
                         <ul>
                             <li class="active">Clothings</li>
                             <li>HandBag</li>
@@ -204,102 +205,36 @@
                         </ul>
                     </div>
                     <div class="product-slider owl-carousel">
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/man-1.jpg" alt="">
-                                <div class="sale">Sale</div>
-                                <div class="icon">
-                                    <i class="icon_heart_alt"></i>
+                            <?php include "partial/product.php";
+                                while ($produk = mysqli_fetch_array($query)) {
+                            ?>
+                            <div class="product-item">
+                                <div class="pi-pic">
+                                    <img  src="<?= $_ENV['base_url']."img-uploaded/".$produk['thumbnail']?>" alt="">
+                                    <ul>
+                                        <li class="w-icon active"><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+                                        <li class="quick-view"><a href="<?= $_ENV['base_url']?>produk/<?= $produk['slug'] ?>">Lihat Detail</a>
+                                        </li>
+                                        <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
+                                    </ul>
                                 </div>
-                                <ul>
-                                    <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $14.00
-                                    <span>$35.00</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/man-2.jpg" alt="">
-                                <div class="icon">
-                                    <i class="icon_heart_alt"></i>
-                                </div>
-                                <ul>
-                                    <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
+                                <div class="pi-text">
+                                    <div class="catagory-name"><?= $produk['jenis_produk'] ?></div>
+                                    <a href="#">
+                                        <h5><?= $produk['nama_produk'] ?></h5>
+                                    </a>
+                                    <div class="product-price">
+                                        Rp. <?= $produk['harga'] ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/man-3.jpg" alt="">
-                                <div class="icon">
-                                    <i class="icon_heart_alt"></i>
-                                </div>
-                                <ul>
-                                    <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/man-4.jpg" alt="">
-                                <div class="icon">
-                                    <i class="icon_heart_alt"></i>
-                                </div>
-                                <ul>
-                                    <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+                                <?php } ?>
                     </div>
                 </div>
                 <div class="col-lg-3 offset-lg-1">
-                    <div class="product-large set-bg m-large" data-setbg="img/products/man-large.jpg">
-                        <h2>Men’s</h2>
-                        <a href="#">Discover More</a>
+                    <div class="product-large set-bg m-large" data-setbg="img/tentang1.jpg">
+                        <h2>Ida Batik</h2>
+                        <a href="http://localhost/ws-web-kelompok4/idabatik-web/galeri/">Discover More</a>
                     </div>
                 </div>
             </div>
@@ -309,40 +244,40 @@
 
     <!-- Instagram Section Begin -->
     <div class="instagram-photo">
-        <div class="insta-item set-bg" data-setbg="img/insta-1.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-15/e35/28434593_164416587550253_5680107760927113216_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=105&_nc_ohc=S5I07cNPJnAAX-hr1fo&tp=1&oh=e0f927c96173b58995253ea972e185ac&oe=60074AD8">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idabatikbondowoso</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" data-setbg="img/insta-2.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/90064805_626090854622645_1307955299742785251_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=105&_nc_ohc=79GiknaUMWwAX-FhfsJ&tp=1&oh=fd1ca60a3e63c4cff72c0a4bf269b20e&oe=600508D3">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idabatikbondowoso</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" data-setbg="img/insta-3.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-15/e35/47168515_749592598729735_1693924354012393428_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=107&_nc_ohc=WOrs7hJ5slwAX-2R4rf&tp=1&oh=888fc3cc35a357b88c78bf71cd13cc92&oe=60064201">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idabatikbondowoso</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" data-setbg="img/insta-4.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-15/e35/88181670_192883271941268_5097711966474785998_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=108&_nc_ohc=-jr_1FyosPAAX8Ax-TS&tp=1&oh=79066e2e0b2c2dfbc82a2a8918751839&oe=60064B41">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idabatikbondowoso</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" data-setbg="img/insta-5.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/89434099_2668022563421055_4283339470004251043_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=103&_nc_ohc=GBhBI7x2U_kAX-llODu&tp=1&oh=a4488ba9e4adef6ede0196bb85694c17&oe=60063B7F">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idabatikbondowoso</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" data-setbg="img/insta-6.jpg">
+        <div class="insta-item set-bg" data-setbg="https://scontent-sin6-3.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/89710538_661130037976762_103994847833051677_n.jpg?_nc_ht=scontent-sin6-3.cdninstagram.com&_nc_cat=110&_nc_ohc=4tzOHOkE-gwAX9K9ynw&tp=1&oh=589868dbaf2f3d078964b85a1a959cf2&oe=60073874">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
-                <h5><a href="#">colorlib_Collection</a></h5>
+                <h5><a href="https://www.instagram.com/idabatikbondowoso/">idaBatik_collection</a></h5>
             </div>
         </div>
     </div>
@@ -466,5 +401,5 @@
 
     <?php include "partial/footer.php" ?>
 </body>
-
+    <script async src="//www.instagram.com/embed.js"></script>  
 </html>
